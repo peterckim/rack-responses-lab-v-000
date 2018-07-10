0f530dba9f492920a214eea0ad4685161fcef0e6
Peter Kim <peterckim96@gmail.com>
2018-07-10 00:16:49 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,12 @@
+class Application
+  def call(env)
+    resp = Rack::Response.new
+    
+    current_time = Time.new
+    if current_time.hour < 12
+      resp.write "Good Morning!"
+    else
+      resp.write "Goof Afternoon!"
+    end
+  end
+end
