88352ee5172e7568aa2de1c4d5975422bcf475ad
Peter Kim <peterckim96@gmail.com>
2018-07-10 00:20:45 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     
     current_time = Time.new
     if current_time.hour.to_i < 12
-      resp.write "#{current_time.hour}"
+      resp.write "#{current_time.inspect}"
       resp.write "Good Morning!"
     else
       resp.write "#{current_time.hour}"
