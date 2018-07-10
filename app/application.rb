f3aa7f16a6a61d100a4c9323d6a8814a726b80eb
Peter Kim <peterckim96@gmail.com>
2018-07-10 00:18:05 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
     resp = Rack::Response.new
     
     current_time = Time.new
-    if current_time.hour < 12
+    if current_time.hour <= 12
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
